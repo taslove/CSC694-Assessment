73bb3b3a135da7609292bcf1bccc08a2b0606d2b
unknown <cstclair@noctrl.edu>
2014-01-23 13:41:47 -0600

    project skeleton

---
+++ zendframework-skeleton/module/Outcomes/Module.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Outcomes;
+
+use Outcomes\Model\Outcomes;
+use Outcomes\Model\OutcomesTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+
+class Module
+{
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getServiceConfig()
+    {
+        /*return array(
+            'factories' => array(
+                'Outcomes\Model\OutcomesTable' => function($sm) {
+                    $tableGateway = $sm->get('OutcomesTableGateway');
+                    $table = new OutcomesTable($tableGateway);
+                    return $table;
+                },
+                'OutcomesTableGateway' => function ($sm) {
+                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Outcomes());
+                    return new TableGateway('Outcomes', $dbAdapter, null, $resultSetPrototype);
+                },
+            ),
+        );
+        */
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
