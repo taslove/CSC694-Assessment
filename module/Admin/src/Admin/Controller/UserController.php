4e4bf0818bbc5363f321db6ee70b16935a2dc384
cstclair <cstclair@noctrl.edu>
2014-02-06 19:00:28 -0600

    initial structure

---
+++ module/Admin/src/Admin/Controller/UserController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Admin\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class UserController extends AbstractActionController
+{
+   protected $studentTable;
+
+   public function indexAction()
+   {
+      echo "List all users here";  
+   }
+   public function addAction()
+   {
+      echo "Add User";  
+   }
+   public function editAction()
+   {
+      echo "Edit User";  
+   }
+   public function deleteAction()
+   {
+      echo "Delete User";  
+   }
+
+}
