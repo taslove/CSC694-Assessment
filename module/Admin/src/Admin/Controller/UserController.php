b4a70b08ee73829c968e2aa881bc11fff2066b31
Jack <jwgregory@noctrl.edu>
2014-02-23 15:42:01 -0800

    added pagniation code to user controller

--- module/Admin/src/Admin/Controller/UserController.php
+++ module/Admin/src/Admin/Controller/UserController.php
@@ -16,6 +16,8 @@ use Zend\View\Model\ViewModel;
 use Zend\Db\Sql\Select;
 use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\Adapter\Adapter;
+use Zend\Paginator\Paginator;
+use Zend\Paginator\Adapter\Iterator as paginatorIterator;
 
 
 class UserController extends AbstractActionController
@@ -24,9 +26,28 @@ class UserController extends AbstractActionController
 
    public function indexAction()
     {
+       
+       
+        $page = $this->params()->fromRoute('page') ? (int) $this->params()->fromRoute('page') : 1;
+
+        $users = $this->getUserQueries()->fetchAll();
+        $itemsPerPage = 10;
+
+        
+        $paginator = new Paginator($users);
+        $paginator = Zend_Paginator::factory($users);
+        $paginator->setCurrentPageNumber($page)
+                ->setItemCountPerPage($itemsPerPage)
+                ->setPageRange(7);
+
         return new ViewModel(array(
+                    'page' => $page,
+                    'paginator' => $paginator,
+                ));
+       
+      /*  return new ViewModel(array(
             'users' => $this->getUserQueries()->fetchAll(),
-        ));
+        ));*/
     }
    public function addAction()
    {
