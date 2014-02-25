ac21e747f94601f65af6b1bcf82eeeba3e125b6b
Anand Kalelkar <akalelkar@nocrtl.edu>
2014-02-25 14:44:10 -0600

    Cleaning up unneccessary files

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -69,32 +69,6 @@ return array(
                     ),
                 ),
             ),
-            'authenticate' => array(
-                'type'    => 'Literal',
-                'options' => array(
-                    'route'    => '/authenticate',
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'Authenticate\Controller',
-                        'controller'    => 'Index',
-                        'action'        => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'default' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
-                    ),
-                ),
-            ),
         ),
     ),
     'service_manager' => array(
@@ -118,8 +92,7 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController',
-            'Authenticate\Controller\Index' => 'Authenticate\Controller\IndexController',           
+            'Application\Controller\Index' => 'Application\Controller\IndexController',           
         ),
     ),
     'view_manager' => array(
