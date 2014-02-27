6626909fa79ff8a6b6753e4667dfdb4464af33ca
Jack <jwgregory@noctrl.edu>
2014-02-26 20:39:20 -0800

    added programs index view with pagination

--- module/Admin/src/Admin/Model/ProgramTable.php
+++ module/Admin/src/Admin/Model/ProgramTable.php
@@ -6,6 +6,10 @@ use Zend\Db\TableGateway\AbstractTableGateway;
 use Zend\Db\Adapter\Adapter;
 use Zend\Db\Sql\Sql;
 use Zend\Db\Sql\Where;
+use Zend\Db\Sql\Select;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Paginator\Adapter\DbSelect;
+use Zend\Paginator\Paginator;
 
 class ProgramTable extends AbstractTableGateway
 {
@@ -18,10 +22,30 @@ class ProgramTable extends AbstractTableGateway
         $this->initialize();
     }
 
-    public function fetchAll()
+    public function fetchAll($paginated=false)
     {
+         if($paginated) {
+            // create a new Select object for the table album
+            $select = new Select('programs');
+            // create a new result set based on the Album entity
+            $resultSetPrototype = new ResultSet();
+            $resultSetPrototype->setArrayObjectPrototype(new Program());
+            // create a new pagination adapter object
+            $paginatorAdapter = new DbSelect(
+                // our configured select object
+                $select,
+                // the adapter to run it against
+                $this->adapter,    
+                #$this->tableGateway->getAdapter(),
+                // the result set to hydrate
+                $resultSetPrototype
+            );
+            $paginator = new Paginator($paginatorAdapter);
+            return $paginator;
+        }
         $resultSet = $this->select();
         return $resultSet;
+        
     }
 
     public function getProgram($id)
