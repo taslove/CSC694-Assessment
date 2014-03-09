be2e0eca84ef4fb6def63ab7b75d58c196b69e86
cstclair <cstclair@noctrl.edu>
2014-03-08 22:03:48 -0600

    authuser and more queries

--- module/Admin/src/Admin/Controller/QueriesController.php
+++ module/Admin/src/Admin/Controller/QueriesController.php
@@ -18,20 +18,31 @@ use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\Adapter\Adapter;
 use Zend\Paginator\Adapter\ArrayAdapter;
 use Zend\Paginator\Paginator;
+use Zend\Session\Container;
+use Application\Authentication\AuthUser;
 
 class QueriesController extends AbstractActionController
 {
    protected $tableResults;
+   
+   public function onDispatch(\Zend\Mvc\MvcEvent $e) 
+   {    $validUser = new AuthUser();
+        if (!$validUser->Validate()){
+            return $this->redirect()->toRoute('application');
+        }
+        else{
+            return parent::onDispatch( $e );
+        }
+   }
 
    public function indexAction()
    {
-    //  $form = new QueriesForm();
-      return new ViewModel();
+       return new ViewModel();
    }
  
    // Show programs that don't have any plans for a specific year 
-   public function getQuery1Action(){
-      
+   public function getQuery1Action()
+   {
       $resultsarray = '';
       
       // get year from route
@@ -145,18 +156,46 @@ class QueriesController extends AbstractActionController
       $currentYear = date('Y', time());
       
       // determine current school year
-      // year in plans is spring term year 2013-2014 school year is entered as 2014
+      // year in plans table is spring term year (2013-2014 school year is entered as 2014)
       if ($currentMonth > 8){
          $currentYear = $currentYear + 1;
       }
-      $results = $this->getUserQueries()->getProgramsWithModifiedPreviousYearPlans($currentYear);
+      $results = $this->getUserQueries()->getProgramsModifiedLastYearsPlans($currentYear);
       
       // iterate over database results forming a php array
       foreach ($results as $result){
           $resultsarray[] = $result;
       }
       // get program names
-      $partialView = new ViewModel(array('querytitle' => 'Programs With Modified Previous Year Plans ',
+      $partialView = new ViewModel(array('querytitle' => 'Programs That Modified Last Year\'s Plans ',
+                                         'programs' => $resultsarray));
+      $partialView->setTerminal(true);
+      return $partialView;
+   }
+   
+   // Show programs that have added or modified a report for a previous year
+   public function getQuery7Action(){
+      
+      $resultsarray = '';
+      
+      // determine current school year
+      date_default_timezone_set('America/Chicago');
+      $currentMonth = date('m', time());
+      $currentYear = date('Y', time());
+      
+      // determine current school year
+      // year in plans table is spring term year (2013-2014 school year is entered as 2014)
+      if ($currentMonth > 8){
+         $currentYear = $currentYear + 1;
+      }
+      $results = $this->getUserQueries()->getProgramsModifiedLastYearsReports($currentYear);
+      
+      // iterate over database results forming a php array
+      foreach ($results as $result){
+          $resultsarray[] = $result;
+      }
+      // get program names
+      $partialView = new ViewModel(array('querytitle' => 'Programs That Modified Last Year\'s Reports ',
                                          'programs' => $resultsarray));
       $partialView->setTerminal(true);
       return $partialView;
@@ -180,31 +219,6 @@ class QueriesController extends AbstractActionController
                     
         }
         return $this->tableResults;
-    }
-    
-    // Creates list of available units (departments/programs)
-    // based on user role and privileges.
-    public function getUnitsAction()
-    {
-        // get action from id in url
-        $actionChosen = $this->params()->fromRoute('id', 0);
-     
-        // get units for that action
-        if ($actionChosen == 'View'){
-            $results = $this->getGenericQueries()->getUnits();
-        }
-        else{
-            $results = $this->getGenericQueries()->getUnitsByPrivId($this->userID);
-        }
-      
-        // iterate through results forming a php array
-        foreach ($results as $result){
-            $unitData[] = $result;
-        }
-      
-        // encode results as json object
-        $jsonData = new JsonModel($unitData);
-        return $jsonData;
     }
    
 }
