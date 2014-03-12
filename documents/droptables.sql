c580860e67501c7e1c61ebbb645d144894b86534
Anand Kalelkar <akalelkar@nocrtl.edu>
2014-03-12 17:13:21 -0500

    cleanup

--- documents/droptables.sql
+++ documents/droptables.sql
@@ -1,14 +1,15 @@
-USE testassess;
-drop table liaison_privs;
-drop table unit_privs;
-drop table plan_outcomes;
-drop table report_documents;
-drop table plan_documents;
-drop table meta_plans;
-drop table reports;
-drop table plans;
-drop table outcomes;
-drop table programs;
-drop table units;
-drop table user_roles;
-drop table users;
+User ID, not user name
+
+please log in
+att title "enter north central user id"
+
+spacing between user box pass box
+
+find out how to make form elements vertical
+
+logout button - conditional
+	logout action
+
+authentication message if failed
+
+
